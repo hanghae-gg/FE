12ad07ef11bebb442e1081092bd1f60842af6f36
Jungki <andy960609@gmail.com>
2023-01-25 21:09:58 +0900

    리스트, 게시글 , 댓글 CRUD 값 가져오기

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,200 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import "./../App.css";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+// import Layout from "../shared/Layout";
+// import Love from "./love/Love";
+// import Rank from "./rank/Rank";
 
-export default function List() {
+// 토큰 값 로컬스토리지에서 지정가져오기
+const token = localStorage.getItem("Authorizationtest");
+
+const Index = () => {
+  const navigate = useNavigate();
+  const [lists, setLists] = useState([]);
+
+  const fetchList = async () => {
+    const { data } = await axios.get(`${process.env.REACT_APP_LIST}/posts`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    setLists(data);
+  };
+
+  const onClickDeleteLists = async (id) => {
+    const result = window.confirm("삭제하시겠습니까?");
+    if (!token) {
+      return alert("로그인을 해주세요");
+    } else {
+      if (result) {
+        await axios.delete(`${process.env.REACT_APP_LIST}/posts/${id}`, {
+          headers: {
+            Authorization: token,
+          },
+        });
+      } else {
+        return;
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return (
-    <div>
-      <h1>gi</h1>
-    </div>
+    <StContent>
+      <div>
+        <Header>
+          <Btn
+            onClick={() => {
+              window.localStorage.clear();
+              navigate("/");
+            }}
+          >
+            로그아웃
+          </Btn>
+          <Btn
+            onClick={() => {
+              navigate("/Add");
+            }}
+          >
+            게시글 작성하기
+          </Btn>
+        </Header>
+      </div>
+      <Listt>
+        {lists?.map((list) => {
+          return (
+            <StOneListBox key={list.postId}>
+              <div
+                key={list.postId}
+                onClick={() => {
+                  navigate(`/Detail/${list.postId}`);
+                }}
+              >
+                <StImage src={list.imageUrl} />
+              </div>
+              <div>
+                <StOneList>
+                  <br />
+                  <div>
+                    <span>
+                      <Unit>{list.title}</Unit>
+                    </span>
+                  </div>
+                  <br />
+                  <div>
+                    <StButton
+                      type="button"
+                      onClick={() => {
+                        onClickDeleteLists(list.postId);
+                      }}
+                    >
+                      삭제하기
+                    </StButton>
+                  </div>
+                </StOneList>
+              </div>
+            </StOneListBox>
+          );
+        })}
+      </Listt>
+    </StContent>
   );
-}
+};
+
+export default Index;
+
+const StOneListBox = styled.div`
+  margin-left: 20px;
+  background-color: #dadada;
+  /* border: 1px solid gray; */
+  padding: 15px;
+  border-radius: 20px;
+  width: 300px;
+  height: fit-content;
+`;
+
+const StOneList = styled.div``;
+
+const Header = styled.div`
+  margin-top: 80px;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  margin-bottom: 24px;
+`;
+
+const Btn = styled.button`
+  width: 170px;
+  background-color: #343434;
+  border: none;
+  color: white;
+  align-items: center;
+  min-height: 10px;
+  border-radius: 25px;
+  padding: 15px;
+  margin: auto;
+  font-weight: bold;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: 15px;
+`;
+
+const Listt = styled.div`
+  height: fit-content;
+  max-width: 1440px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  place-items: center;
+  gap: 70px 0px;
+  margin-top: 35px;
+`;
+
+const Unit = styled.div`
+  text-align: center;
+  font-size: 30px;
+  line-height: 20px;
+  color: #000000;
+`;
+const StButton = styled.button`
+  margin: auto;
+  background-color: black;
+  margin-top: 10px;
+  text-align: center;
+  width: 70px;
+  height: 30px;
+  opacity: 0.9;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  border: 1px solid black;
+  font-weight: bold;
+  font-size: 10px;
+  color: white;
+  cursor: pointer;
+  /* font-family: "Noto Sans KR", sans-serif; */
+`;
+const StImage = styled.img`
+  width: 240px;
+  height: 240px;
+  justify-content: center;
+  margin: auto;
+  display: flex;
+  align-items: center;
+`;
+
+const StContent = styled.div`
+  display: flex;
+  justify-content: center;
+`;
--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,9 +1,218 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import styled from "styled-components";
 
-export default function Write() {
+const Add = () => {
+  const navigate = useNavigate();
+  const [lists, setLists] = useState({
+    title: "",
+    content: "",
+    image: "",
+    // visit: 0,
+  });
+
+  // 토큰 값 로컬스토리지에서 지정가져오기
+  const token = localStorage.getItem("Authorizationtest");
+
+  //이미지 미리보기와 파일첨부 기능
+  const [imgBase64, setImgBase64] = useState([]); // 파일 base64
+  const [imgFile, setImgFile] = useState(null); //파일
+  //미리보기
+  const handleChangeFile = (event) => {
+    setImgFile(event.target.files);
+    setImgBase64([]);
+    for (var i = 0; i < event.target.files.length; i++) {
+      if (event.target.files[i]) {
+        let reader = new FileReader();
+        reader.readAsDataURL(event.target.files[i]);
+        reader.onloadend = () => {
+          const base64 = reader.result;
+          if (base64) {
+            var base64Sub = base64.toString();
+            setImgBase64((imgBase64) => [...imgBase64, base64Sub]);
+          }
+        };
+      }
+    }
+  };
+  //그림파일 폼데이터로 보내기
+  const onWriteHandler = async (lists) => {
+    const fd = new FormData();
+    Object.values(imgFile).forEach((file) => fd.append("file", file));
+    fd.append("title", lists.title);
+    fd.append("content", lists.content);
+    await axios
+      .post(`${process.env.REACT_APP_LIST}/posts`, fd, {
+        headers: {
+          Authorization: token,
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        if (response.data) {
+        }
+      })
+      .catch((error) => {});
+    navigate("/List");
+  };
+
   return (
-    <div>
-      <h1>gi</h1>
-    </div>
+    <StCenter>
+      <StWhole>
+        <StTitleBox>게시글 작성하기</StTitleBox>
+        <StAddCard>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              onWriteHandler(lists);
+            }}
+          >
+            <StInputBox>
+              <br />
+              <StInput
+                fullwidth
+                type="text"
+                onChange={(event) => {
+                  const { value } = event.target;
+                  setLists({
+                    ...lists,
+                    listName: value,
+                  });
+                }}
+                placeholder="제목 : "
+                value={lists.listName}
+                name="listName"
+                required
+              />
+              <StInput
+                type="file"
+                accept="image/jpg,image/png,image/jpeg,image/gif"
+                onChange={handleChangeFile}
+                multiple="multiple"
+              />
+              {imgBase64.map((item) => {
+                return (
+                  <img
+                    key={Date.now()}
+                    src={item}
+                    alt="First slide"
+                    style={{ width: "100%", height: "100%" }}
+                  />
+                );
+              })}
+              <Textarea
+                name="text"
+                onChange={(event) => {
+                  const { value } = event.target;
+                  setLists({
+                    ...lists,
+                    text: value,
+                  });
+                }}
+                value={lists.text}
+                rows="10"
+                maxLength={200}
+                placeholder="게시글 설명 : "
+                required
+              />
+            </StInputBox>
+            <StButtons>
+              <StButton type="submit">저장하기</StButton>
+              <StButton
+                onClick={() => {
+                  navigate(-1);
+                }}
+              >
+                뒤로가기
+              </StButton>
+            </StButtons>{" "}
+          </form>
+        </StAddCard>
+      </StWhole>
+    </StCenter>
   );
-}
+};
+
+export default Add;
+
+// 전체중앙정렬
+const StCenter = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+// textarea 크기고정(width 100%)및 css
+const Textarea = styled.textarea`
+  margin-top: 10px;
+  width: 200px;
+  border: 1px solid #eee;
+  padding: 12px;
+  font-size: 14px;
+  border-radius: 20px;
+  width: 300px;
+`;
+const StAddCard = styled.div`
+  padding: 50px 0 30px 0;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  background-color: #dadada;
+  width: 400px;
+  height: fit-content;
+  border-radius: 60px;
+`;
+
+const StButton = styled.button`
+  margin: 10px auto auto auto;
+  background-color: black;
+  text-align: center;
+  width: 90px;
+  height: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  border: 1px solid black;
+  font-weight: bold;
+  font-size: 13px;
+  color: white;
+  cursor: pointer;
+`;
+const StButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const StWhole = styled.div`
+  background-color: black;
+  margin-top: 100px;
+`;
+const StTitleBox = styled.h2`
+  /* text-align: center; */
+  padding-top: 30px;
+  padding-bottom: 110px;
+  margin-bottom: -110px;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  background-color: #dadada;
+  width: 400px;
+  height: 50px;
+  border-radius: 60px;
+`;
+const StInputBox = styled.div`
+  margin-top: -50px;
+  width: 350px;
+  text-align: center;
+`;
+const StInput = styled.input`
+  width: 300px;
+  height: 20px;
+  border-radius: 15px;
+  border: none;
+  padding: 5px;
+  margin-top: 5px;
+`;
