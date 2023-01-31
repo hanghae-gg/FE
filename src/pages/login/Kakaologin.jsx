b05f5e90ffade86fff2d565848f334a16766885b
Jung Woo <wldnr1208@naver.com>
2023-01-31 16:49:54 +0900

    카카오로그인 테스트

--- src/pages/login/Kakaologin.jsx
+++ src/pages/login/Kakaologin.jsx
@@ -3,9 +3,10 @@ import React from "react";
 export default function Kakaologin() {
   return (
     <div>
-      <a href="https://kauth.kakao.com/oauth/authorize?client_id=bb8c316ab6b257776807176ec5c9609c&redirect_uri=localhost:8080_URL&response_type=code">
+      <a href="https://kauth.kakao.com/oauth/authorize?client_id=bb8c316ab6b257776807176ec5c9609c&redirect_uri=http://hh.gg.s3-website.ap-northeast-2.amazonaws.com/login&response_type=code">
         <img
           src="https://www.ekcls.com/m/image/sub/signup/signup_28.jpg"
+          "https://kauth.kakao.com/oauth/authorize?client_id=REST_API키&redirect_uri=REDIRECT_URL&response_type=code"
           style="height:60px"
         />
       </a>
